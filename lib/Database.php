d62b29dfd45ade6a5d3964c4701a2a3c5007ae81
DataWelder <sszypulski.kess@gmail.com>
2020-01-03 16:03:11 +0100

    Some Changes

--- lib/Database.php
+++ lib/Database.php
@@ -1,13 +1,22 @@
 <?php
 
-    class DataBaseConnect{
+    class Database{
         // specify your own database credentials
         private $host = "localhost";
         private $db_name = "WalutoBase";
         private $username = "root";
         private $password = "";
         public $conn;
-    
+        
+        public function __construct()
+        {
+            $config = include("./config/localhost.php");
+            $this->host = $config['host'];
+            $this->db_name = $config['name'];
+            $this->username = $config['user'];
+            $this->password = $config['pass'];
+        }
+
         // get the database connection
         public function getConnection(){
     
---
+++ up.ps1
@@ -0,0 +1,1 @@
+Copy-Item "C:\Users\sszyp\Desktop\WalutoMiner" -Destination "C:\www\" -Force -Recurse -Verbose
