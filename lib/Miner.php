7d3e997073f02968dde1406c827a391a12a82a18
Sebastian Szypulski <32041785+TechAlchemistPL@users.noreply.github.com>
2020-06-13 21:43:45 +0200

    Update Miner.php

--- lib/Miner.php
+++ lib/Miner.php
@@ -1,6 +1,6 @@
 <?php
 
-class Miner{
+class Miner {
 
     private $conn;
     private $source;
@@ -8,29 +8,29 @@ class Miner{
     private $source_url;
     private $curr_list = array();
 
-    public function __construct($conn, $source)
-    {
+    public function __construct($conn, $source) {
         $this->conn = $conn;
         $this->source = strtolower($source);
         $this->getSource();
     }
 
-    private function getSource(){
+    private function getSource() {
         $sql = 'SELECT id, url FROM s_source WHERE name = :source LIMIT 1;';
         $stmt = $this->conn->prepare($sql);
         $stmt->bindParam(':source', $this->source, PDO::PARAM_STR);
         $result = $stmt->execute();
-        if($result){
+        if($result) {
             $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
             $this->source_id = $data[0]['id'];
             $this->source_url = $data[0]['url'];
             return true;
-        }else{
+        }
+        else {
             return false;
         }
     }
 
-    private function getCurrList(){
+    private function getCurrList() {
         $sql = 'SELECT id, iso_code FROM s_currency';
         $stmt = $this->conn->prepare($sql);
         $result = $stmt->execute();
@@ -43,7 +43,7 @@ class Miner{
     }
 
 
-    private function nbpMining(){
+    private function nbpMining() {
         $json = json_decode(file_get_contents($this->source_url), true);
         $data = $json[0]['rates'];
         $sql = 'INSERT INTO currences(id ,s_currency_id, bid, ask, s_source_id, cur_date) VALUES (null, :curr_id, :bid, :ask, :source_id, CURRENT_TIMESTAMP); ';
@@ -59,22 +59,17 @@ class Miner{
             }
         }
         $this->conn->exec($stmt);
-
     }
 
     
 
-    public function mining(){
+    public function mining() {
         $this->getCurrList();
-
-        switch($this->source){
+        switch($this->source) {
             case 'nbp':
                 $data = $this->nbpMining();
             break;
-        }
-        
+        }    
     }
 }
-
-
 ?>
