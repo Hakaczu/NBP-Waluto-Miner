805d811590df315129dcb91c5b2ef03834a3f37c
DataWelder <sszypulski.kess@gmail.com>
2020-06-13 21:45:50 +0200

    Clean code Database.php

--- lib/Database.php
+++ lib/Database.php
@@ -1,6 +1,6 @@
 <?php
 
-    class Database{
+    class Database {
         // specify your own database credentials
         private $dbtype = 'prod';
         private $host = "localhost";
@@ -8,40 +8,32 @@
         private $username = "root";
         private $password = "";
         
-        public function __construct()
-        {
-            if($this->dbtype == 'prod'){
+        public function __construct() {
+            if($this->dbtype == 'prod') {
                 $config = include("../config/walutobase.php");
                 $this->host = $config['host'];
                 $this->db_name = $config['name'];
                 $this->username = $config['user'];
                 $this->password = $config['pass'];
-            }else{
+            } else {
                 $config = include("../config/localhost.php");
                 $this->host = $config['host'];
                 $this->db_name = $config['name'];
                 $this->username = $config['user'];
                 $this->password = $config['pass'];
             }
-            
         }
 
         // get the database connection
-        public function getConnection(){
-    
-            //$this->conn = null;
-    
-            try{
+        public function getConnection() {   
+            try {
                 $conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
                 $conn->exec("set names utf8");
-            }catch(PDOException $exception){
+            } catch(PDOException $exception) {
                 http_response_code(500);
                 echo $exception;
             }
             return $conn;
         }
-
     }   
-
-
 ?>
